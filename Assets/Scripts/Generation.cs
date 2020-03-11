0f0bd61d8d79b2b5587c7095624e5fdbd7d568fa
jac3rd <jac3rd@virginia.edu>
2020-03-11 04:55:56 -0400

    added generation using rigidbody2d

---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    public int numRooms = 50;
+    public int minSize = 4;
+    public int maxSize = 10;
+    public GameObject Room;
+    // Start is called before the first frame update
+    void Start() {
+        Random.InitState(System.Environment.TickCount);
+        GenerateRooms();
+    }
+
+    void GenerateRooms() {
+        for(int i = 0; i < numRooms; i++) {
+            GameObject room = GameObject.Instantiate(Room);
+            room.GetComponent<BoxCollider2D>().size = new Vector2(Random.Range(minSize,maxSize+1), Random.Range(minSize,maxSize));
+            room.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
