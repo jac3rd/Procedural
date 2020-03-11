8a9d5618487afd7ac4ceb2a57ac9a24a66014bb8
jac3rd <jac3rd@virginia.edu>
2020-03-11 05:30:28 -0400

    added sprites to visualize generated rooms

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -17,14 +17,21 @@ public class Generation : MonoBehaviour
     void GenerateRooms() {
         for(int i = 0; i < numRooms; i++) {
             GameObject room = GameObject.Instantiate(Room);
-            room.GetComponent<BoxCollider2D>().size = new Vector2(Random.Range(minSize,maxSize+1), Random.Range(minSize,maxSize));
+            BoxCollider2D boxCollider2D = room.GetComponent<BoxCollider2D>();
+            boxCollider2D.size = new Vector2(Random.Range(minSize,maxSize+1), Random.Range(minSize,maxSize));
+            room.GetComponent<SpriteRenderer>().size = boxCollider2D.size;
             room.transform.parent = transform;
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        int overlaps = 0;
+        ContactFilter2D contactFilter2D = new ContactFilter2D();
+        for(int i = 0; i < transform.childCount; i++) {
+            Collider2D[] results = new Collider2D[4];
+            overlaps += transform.GetChild(i).GetComponent<Rigidbody2D>().OverlapCollider(contactFilter2D,results);
+        }
+        Debug.Log(overlaps);
     }
 }
